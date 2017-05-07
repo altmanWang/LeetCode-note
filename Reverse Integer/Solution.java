4364da2a0c1b32b15cc1a1082e6c160872f05d18
altmanWang <qx_wang@foxmail.com>
2017-05-07 23:06:26 +0800

    Reverse Integer

---
+++ Reverse Integer/Solution.java
@@ -0,0 +1,20 @@
+public class Solution{ 
+	public int reverse(int x) {
+            int reversed_n = 0;
+            while (x != 0) {
+                int temp = reversed_n * 10 + x % 10;
+                x = x / 10;
+                if (temp / 10 != reversed_n) {
+                    reversed_n = 0;
+                    break;
+                }
+                reversed_n = temp;
+           }
+           return reversed_n;
+    }
+	public static void main(String[] args){
+		int x = -1;
+		Solution s = new Solution();
+		System.out.println(s.reverse(x));
+	}
+}
